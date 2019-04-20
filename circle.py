4d7dea182d622b25fe59191bde9104ee337e1da8
JackonLIu <liuxinpeng.222@foxmail.com>
2019-04-20 23:10:27 +0800

    修改画条形图的方法，截取部分数据

--- circle.py
+++ circle.py
@@ -1,11 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-# 设置字体
-from matplotlib.font_manager import FontProperties
-font = FontProperties(fname = "C:/Windows/Fonts/simsun.ttc", size = 14)
-
-
 # 绘制一个圆形散点图的示例
 t = np.arange(1, 10, 0.05)
 x = np.sin(t)
@@ -16,9 +11,9 @@ plt.figure(figsize = (8, 5))
 plt.plot(x, y, "r-*")
 ##使坐标轴相等
 plt.axis("equal")
-plt.xlabel("正弦", fontproperties = font)
-plt.ylabel("余弦", fontproperties = font)
-plt.title("一个圆形", fontproperties = font)
+plt.xlabel("正弦")
+plt.ylabel("余弦")
+plt.title("一个圆形")
 
 ##显示图像
 plt.show()
--- data_analysis.py
+++ data_analysis.py
@@ -1,13 +1,11 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-csv_data = pd.read_csv('5_chengji.csv')  # 读取训练数据
+csv_data = pd.read_csv('chengji.csv')  # 读取训练数据
 print(csv_data.shape)  # (189, 9)
 N = 50
 csv_batch_data = csv_data.tail(N)  # 取后5条数据
-print(csv_batch_data.shape)  # (5, 9)
-# train_batch_data = csv_batch_data[list(range(3, 6))]  # 取这20条数据的3到5列值(索引从0开始)
-# print(train_batch_data)
+print(csv_batch_data)  # (5, 9)
 ## 条形图
 ## 平均分和标准差
 means_men = (20, 35, 30, 35, 27)
@@ -18,15 +16,29 @@ std_women = (3, 5, 2, 3, 3)
 ##条形图
 fig, ax = plt.subplots()
 ##生成0, 1, 2, 3, ...
-# index = np.arange(csv_batch_data)
+index = np.arange(5)
 bar_width = 0.35 ## 条的宽度
-csv_batch_data[csv_batch_data["mes_sub_name"] =='育才中学'].head()
+className = csv_batch_data['mes_sub_name']
+print(className)
+mes_StudentID = csv_batch_data['mes_StudentID']
 opacity = 0.4
-
+error_config = {'ecolor': '0.3'}
+mes_T_Score = csv_batch_data['mes_T_Score']
+print(mes_T_Score)
+school=csv_batch_data['育才中学' == csv_batch_data['mes_sub_name']].head()
+print(school)
+## 条形图的第一类条
+rects1 = plt.bar(index, mes_T_Score, bar_width,
+                alpha = opacity,
+                color = 'b',
+                yerr = std_men,
+                error_kw = error_config,
+                label = 'score')
 plt.xlabel('Group')
 plt.ylabel('Scores')
 plt.title('Scores by group and gender')
-# plt.xticks(index + bar_width, ('A', 'B', 'C', 'D', 'E'))
+plt.xticks(index + bar_width, ('A', 'B', 'C', 'D', 'E'))
+
 plt.legend()    # 显示标注
 ## 自动调整subplot的参数给指定的填充区
 plt.tight_layout()
